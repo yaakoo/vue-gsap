6691cb420bbe6bf8c098eb1b8d2e00eab68defbc
smwilk <minami.wilkins@gmail.com>
2022-02-04 13:20:36 +0900

    Remove animation code

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,67 +1,28 @@
 <template>
-    <transition
-        appear
-        name="fade"
-        @before-enter="headerBeforeEnter"
-        @enter="headerEnter"
-    >
-        <div class="header">
-            <div class="header-title">
-                <span class="header-title-text">Vue x GSAP</span>
-            </div>
-            <transition
-                appear
-                @enter="linksEnter"
-            >
-                <div class="links-container">
-                    <div class="links">
-                        <a href="#">Link 1</a>
-                        <a href="#">Link 2</a>
-                        <a href="#">Link 3</a>
-                    </div>
-                </div>
-            </transition>
+    <div class="header">
+        <div class="header-title">
+            <span class="header-title-text">Vue x GSAP</span>
         </div>
-    </transition>
+        <transition
+            appear
+            @enter="linksEnter"
+        >
+            <div class="links-container">
+                <div class="links">
+                    <a href="#">Link 1</a>
+                    <a href="#">Link 2</a>
+                    <a href="#">Link 3</a>
+                </div>
+            </div>
+        </transition>
+    </div>
 </template>
 
 <script>
-import gsap from "gsap"
 
 export default {
     setup() {
-        const headerBeforeEnter = (el) => {
-            gsap.set(el, {
-                y: "-100%",
-                opacity: 0
-            })
-        }
-
-        // ヘッダーが上から落ちてくるアニメーション
-        const headerEnter = (el, done) => {
-            gsap.to(el, {
-                opacity: 1,
-                duration: 1,
-                y: "0",
-                ease: "Power0.easeOut",
-                onComplete: done
-            })
-        }
-
-        const linksBeforeEnter = (el) => {
-            el.style.opacity = 0
-        }
-
-        // リンクがフェイドインするアニメーション
-        const linksEnter = (el, done) => {
-            gsap.to(el, {
-                duration: 1,
-                opacity: 1,
-                delay: 1,
-                onComplete: done
-            })
-        }
-        return { headerBeforeEnter, linksBeforeEnter, headerEnter, linksEnter }
+        // アニメーションコードをここに追加   
     }
 }
 </script>
